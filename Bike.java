51a5c4d86dce0301e82cb4e87cc92abd273d7526
fogmogreal <narcizo.marquess@gmail.com>
2024-03-15 09:59:11 -0300

    atualização

---
+++ Bike.java
@@ -0,0 +1,10 @@
+
+public class Bike {
+
+    public static String marca;
+    public int marchaAtual = 1;
+
+    public void alterarMarcha(int novaMarcha) {
+        marchaAtual = novaMarcha;
+    }
+}
