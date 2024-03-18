f747214710e45088a4c77e8c94c7b38c2997ce99
fogmogreal <narcizo.marquess@gmail.com>
2024-03-18 11:26:44 -0300

    update2

--- Pessoa.java
+++ Pessoa.java
@@ -4,4 +4,23 @@ public class Pessoa {
     public String nome;
     public int idade;
 
+    public String toString() {
+        return "Nome: " + nome + " Idade: " + idade;
+    }
+
+    public Pessoa(String nomeParam) {
+
+        nome = nomeParam;
+
+    }
+
+    public Pessoa(String nomeParam, int idadeParam) {
+        nome = nomeParam;
+        idade = idadeParam;
+    }
+
+    public Pessoa() {
+
+    }
+
 }
--- Programa.java
+++ Programa.java
@@ -1,4 +1,25 @@
 
 public class Programa {
 
+    public static void main(String[] args) {
+
+        Pessoa pessoa1 = new Pessoa("Ricardo", 25);
+        // pessoa1.nome = "Ricardo";
+        // pessoa1.idade = 25;
+
+        Pessoa pessoa2 = new Pessoa("Ana", 21);
+        // pessoa2.nome = "Ana";
+        // pessoa2.idade = 21;
+
+        Pessoa pessoa3 = new Pessoa("fog", 20);
+
+        Pessoa pessoa4 = new Pessoa();
+
+        System.out.println(pessoa1);
+        System.out.println(pessoa2);
+        System.out.println(pessoa3);
+        System.out.println(pessoa4);
+
+    }
+
 }
